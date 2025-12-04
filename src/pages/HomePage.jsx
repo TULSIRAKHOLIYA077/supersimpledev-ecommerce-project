ccf30c74b279b775f569f4ec21047e72e71c080d
TULSIRAKHOLIYA077 <radhikarakholiya077@gmail.com>
2025-12-04 12:24:17 +0530

    Use data from the backend to generate the html

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
 import "./HomePage.css";
 import Header from "../components/Header";
 import CheckmarkIcon from "../assets/images/icons/checkmark.png";
-import { products } from "../../starting-code/data/products";
-import axios from 'axios';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
-  axios.get('http://localhost:3000/api/products')
-    .then((response) =>{
-      console.log(response.data);
-    })
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
 
   return (
     <>
@@ -24,10 +26,7 @@ const HomePage = () => {
             return (
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
@@ -37,12 +36,18 @@ const HomePage = () => {
                 <div className="product-rating-container">
                   <img
                     className="product-rating-stars"
-                      src={`images/ratings/rating-${product.rating.stars * 10}.png`}
+                    src={`images/ratings/rating-${
+                      product.rating.stars * 10
+                    }.png`}
                   />
-                  <div className="product-rating-count link-primary">{product.rating.count}</div>
+                  <div className="product-rating-count link-primary">
+                    {product.rating.count}
+                  </div>
                 </div>
 
-                <div className="product-price">${(product.priceCents/100).toFixed(2)}</div>
+                <div className="product-price">
+                  ${(product.priceCents / 100).toFixed(2)}
+                </div>
 
                 <div className="product-quantity-container">
                   <select>
