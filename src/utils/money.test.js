c70d3b381ef5a39ef568051271c425179d9946c4
TULSIRAKHOLIYA077 <radhikarakholiya077@gmail.com>
2025-12-26 15:05:22 +0530

    Exercise for testing Product's other feature

--- src/utils/money.test.js
+++ src/utils/money.test.js
@@ -11,6 +11,15 @@ describe("formateMoney", () => {
     expect(formatMoney(1090)).toBe("$10.90");
     expect(formatMoney(100)).toBe("$1.00");
   });
+
+  it("Works with the number 0", () => {
+    expect(formatMoney(0)).toBe("$0.00");
+  });
+
+  it('works with negative numbers', () => {
+    expect(formatMoney(-999)).toBe('-$9.99');
+    expect(formatMoney(-100)).toBe('-$1.00');
+  });
 });
 
 //Unit test :-
